916a02a609af6dc125b0a82215adb94858f4d597
Thomas Bytheway <tb403@cam.ac.uk>
2017-09-28 10:19:08 +0100

    Add help text for cute

--- yutu.py
+++ yutu.py
@@ -16,13 +16,18 @@ async def highfive(ctx):
     await ctx.send('{0.mention} :pray: {1.mention}'.format(ctx.me, ctx.author))
 
 @client.command()
-async def cute(ctx, member: discord.Member = None):
-    if member is None:
+async def cute(ctx, user: discord.Member = None):
+    """
+    Tell someone they are cute!
+
+    Tells a user that you think they are cute, if you don't give a user, then Yutu will let you know that you are cute.
+    """
+    if user is None:
         first = ctx.me
         second = ctx.author
     else:
         first = ctx.author
-        second = member
+        second = user
     post = discord.Embed(description='**{0.display_name}** thinks that **{1.display_name}** is cute!'.format(first, second))
     post.set_image(url="https://i.imgur.com/MuVAkV2.gif")
     await ctx.send(embed=post)
