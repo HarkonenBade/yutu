41f93b160a027feea6df6e537da8df0f7ff37155
HarkonenBade <github@harkonen.net>
2017-09-30 00:38:29 +0100

    Add kiss and move interacts to thier own cog

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,7 +1,8 @@
-from . import fluffy_bunnies, meme, misc
+from . import fluffy_bunnies, interact, meme, misc
 
 
 def setup(bot):
+    bot.add_cog(interact.Interact())
     bot.add_cog(meme.Meme())
     bot.add_cog(misc.Misc())
     bot.add_cog(fluffy_bunnies.SoulPact())
--- cogs/misc.py
+++ cogs/misc.py
@@ -1,5 +1,4 @@
 import hashlib
-import random
 
 import discord
 from discord.ext import commands
@@ -28,69 +27,3 @@ class Misc:
                                                                                               rate/10.0)
         msg.set_thumbnail(url=ctx.me.avatar_url)
         await ctx.send(embed=msg)
-
-def interact_fwrk(name, text, help, aliases=[], images=None, disallow_none=False):
-    @commands.command(name=name, aliases=aliases)
-    async def cmd(self, ctx: commands.Context, user: discord.Member = None):
-        if disallow_none:
-            if user is None:
-                await ctx.send("{0.mention}: I'm sorry I don't know that person".format(ctx.author))
-                return
-            else:
-                first = ctx.author
-                second = user
-        else:
-            if user is None:
-                first = ctx.me
-                second = ctx.author
-            else:
-                first = ctx.author
-                second = user
-        post = discord.Embed()
-        if isinstance(text, list):
-            post.description = random.choice(text).format(first, second)
-        else:
-            post.description = text.format(first, second)
-        if images is not None:
-            post.set_image(url=random.choice(images))
-        post.set_thumbnail(url=first.avatar_url)
-        await ctx.send(embed=post)
-    cmd.__doc__ = help
-    setattr(Misc, name, cmd)
-
-interact_fwrk(name='cute',
-              text='**{0.display_name}** thinks that **{1.display_name}** is cute!',
-              help="Tell someone they are cute!",
-              images=['https://i.imgur.com/MuVAkV2.gif'])
-
-interact_fwrk(name='hug',
-              text='**{0.display_name}** gives **{1.display_name}** a warm hug',
-              help="Give someone a hug",
-              aliases=['hugs'],
-              images=['https://i.imgur.com/RDdGYgK.gif'])
-
-interact_fwrk(name='nibble',
-              text='**{0.display_name}** nibbles on **{1.display_name}**',
-              help='Nibble on someone',
-              aliases=['nibbles'],
-              disallow_none=True)
-
-interact_fwrk(name='pat',
-              text='**{0.display_name}** pats **{1.display_name}** gently',
-              help='Pat someone')
-
-interact_fwrk(name='cuddle',
-              text='**{0.display_name}** cuddles around **{1.display_name}**',
-              help='Give someone a cuddle')
-
-interact_fwrk(name='noogie',
-              text='**{0.display_name}** gives **{1.display_name}** a noogie',
-              help='Give someone a noogie',
-              images=['https://imgur.com/a/uBOvN'],
-              disallow_none=True)
-
-interact_fwrk(name='siton',
-              text='**{0.display_name}** sits down on-top of **{1.display_name}**',
-              help='Sit on someone',
-              aliases=['sit-on'],
-              disallow_none=True)
