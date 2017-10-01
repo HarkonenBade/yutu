d15e1c8cbd21b37bacc4e3ce2cc70bfa1677ce78
HarkonenBade <github@harkonen.net>
2017-10-01 14:02:00 +0100

    Add commend

--- cogs/misc.py
+++ cogs/misc.py
@@ -27,3 +27,21 @@ class Misc:
                                                                                               rate/10.0)
         msg.set_thumbnail(url=ctx.me.avatar_url)
         await ctx.send(embed=msg)
+
+    @commands.command()
+    async def commend(self, ctx: commands.Context, user: discord.Member, *args):
+        """
+        Commend a user, with optional reason
+        """
+        reason = await commands.clean_content().convert(ctx, " ".join(args))
+        embed = discord.Embed()
+        if reason:
+            embed.description = (":trophy::military_medal: "
+                                 "**{0.display_name}** commends **{1.display_name}**"
+                                 " :military_medal::trophy:\n"
+                                 "for {2}").format(ctx.author, user, reason)
+        else:
+            embed.description = (":trophy::military_medal: "
+                                 "**{0.display_name}** commends **{1.display_name}**"
+                                 " :military_medal::trophy:").format(ctx.author, user)
+        await ctx.send(embed=embed)
