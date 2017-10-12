404ac38d123610ed73291982a93f0c2f0f42f346
HarkonenBade <github@harkonen.net>
2017-10-12 15:34:27 +0100

    Added ~changelog command

--- cogs/management.py
+++ cogs/management.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+from agithub.GitHub import GitHub
 
 def can_manage():
     def pred(ctx: commands.Context):
@@ -22,3 +23,15 @@ class Manage:
         msg = "Request from {}:\n".format(ctx.author.mention) + feature
         await ctx.bot.get_user(ctx.bot.owner_id).send(content=msg)
         await ctx.message.add_reaction("✅")
+
+    @commands.command()
+    async def changelog(self, ctx: commands.Context):
+        """
+        Get a log of what has changed in Yutu
+        """
+        status, commits = GitHub().repos.harkonenbade.yutu.commits.get(per_page=10)
+        if status == 200:
+            await ctx.send(content="```Changelog:\n{}```".format("\n".join(["- {}".format(c['commit']['message'])
+                                                                            for c in commits])))
+        else:
+            await ctx.send(content="Error: Cannot reach github")
