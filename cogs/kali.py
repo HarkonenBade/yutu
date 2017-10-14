28c0d471ffc201734f5ad1a80b9de30759a372c7
Thomas Bytheway <github@harkonen.net>
2017-10-14 23:39:18 +0100

    Added ~kaligayy

--- cogs/kali.py
+++ cogs/kali.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 
 TAKE_A_MOMENT = "https://i.imgur.com/4rM92UL.gif"
+THATS_GAY = "https://i.imgur.com/w8tbJc5.gif"
 KALI = 142764404872708096
 
 class KaliCommands:
@@ -30,3 +31,15 @@ class KaliCommands:
         await kali.send(random.choice(["No Kali, don't drink the coffee",
                                        "Not with the bean juice Kali",
                                        "None Coffee - Left NoVibrate"]))
+
+    @commands.command()
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def kaligayy(self, ctx: commands.Context):
+        """
+        Kali that's gay
+        """
+        kali = ctx.bot.get_user(KALI)
+        post = discord.Embed()
+        post.set_image(url=THATS_GAY)
+        await ctx.send(content="{0.mention} that's gay.".format(kali),
+                       embed=post)
