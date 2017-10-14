0d4e3cc2e3159bd6ce85a619fc4c7f7c0f919a3d
Thomas Bytheway <github@harkonen.net>
2017-10-14 22:34:41 +0100

    Add ~imlewd command

--- cogs/self_management.py
+++ cogs/self_management.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 from discord.utils import get
 from discord.ext import commands
@@ -35,6 +37,36 @@ class SelfManagement:
         else:
             await ctx.author.add_roles(spoild, reason="Added by ~spoilme command.")
             await ctx.send("Ok {0.mention}, Granting you access to #rwby_spoilers and #rwde_spoilers".format(ctx.author))
+
+    @commands.command()
+    async def imlewd(self, ctx: commands.Context):
+        """
+        Grants you access to the NSFW channels
+        """
+        DECLARATION = "I assert that I am over the age of 18"
+        if get(ctx.guild.roles, name="no lewd") in ctx.author.roles:
+            await ctx.send("{0.mention}, you are not allowed to give yourself the nwde role.".format(ctx.author))
+            return
+
+        nwde = get(ctx.guild.roles, name="im nwde")
+        if nwde in ctx.author.roles:
+            await ctx.send("But {0.mention}, you are already nwde.".format(ctx.author))
+        else:
+            await ctx.send("Please confirm you are over 18. "
+                           "Please reply to this message with the following declaration."
+                           "\"{}\"".format(DECLARATION))
+
+            try:
+                def check(msg):
+                    return (msg.author == ctx.author and
+                            msg.channel == ctx.channel and
+                            DECLARATION in msg.content)
+                await ctx.bot.wait_for("message", check=check, timeout=30)
+            except asyncio.TimeoutError:
+                await ctx.send("Your request has timed out, please try again.")
+            else:
+                await ctx.author.add_roles(nwde, reason="Added by ~imlewd command.")
+                await ctx.send("Ok {0.mention}, Granting you access to #nwde and #dickposting".format(ctx.author))
 
     @commands.command()
     async def pronouns(self, ctx: commands.Context, *, pronouns):
