3f4f1327e4ade3fb1fc9cbd01a74c5ff78815ef9
HarkonenBade <github@harkonen.net>
2019-02-14 15:33:25 +0000

    First pass archive

--- cogs/management.py
+++ cogs/management.py
@@ -1,5 +1,9 @@
 import collections
 import itertools
+import io
+import json
+import gzip
+import datetime
 
 import discord
 from discord.ext import commands
@@ -65,3 +69,69 @@ class Manage:
             for chk in grouper(10, msg_ranks):
                 await ctx.send(content="```{}```".format("\n".join(chk)))
             await tracking.delete()
+
+    @commands.command(hidden=True)
+    @can_manage()
+    async def archive(self, ctx: commands.Context):
+        class Enc(json.JSONEncoder):
+            def default(self, o):
+                if isinstance(o, discord.Message):
+                    ret = {
+                        "tts": o.tts,
+                        "author": o.author,
+                        "content": o.content,
+                        #"embeds": o.embeds,
+                        "id": o.id,
+                        "attachments": o.attachments,
+                        "pinned": o.pinned,
+                        "reactions": o.reactions,
+                        "created_at": o.created_at.isoformat(),
+                    }
+                    if o.edited_at is not None:
+                        ret["edited_at"] = o.edited_at.isoformat()
+                    return ret
+                elif isinstance(o, discord.Member):
+                    return {
+                        "id": o.id,
+                        "joined_at": o.joined_at.isoformat(),
+                        "display_name": o.display_name,
+                        "avatar": o.avatar_url,
+                        "username": o.name + "#" + o.discriminator,
+                    }
+                elif isinstance(o, discord.Attachment):
+                    return {
+                        "id": o.id,
+                        "url": o.url,
+                    }
+                elif isinstance(o, discord.Reaction):
+                    return {
+                        "emoji": o.emoji,
+                        "count": o.count,
+                    }
+                elif isinstance(o, discord.Emoji):
+                    return {
+                        "id": o.id,
+                        "name": o.name,
+                        "url": o.url,
+                    }
+                return super().default(o)
+
+        chname = ctx.channel.name
+        async with ctx.typing():
+            await ctx.message.delete()
+            status = await ctx.send(content="Archiving {}...".format(chname))
+            out = io.BytesIO()
+            stream = gzip.GzipFile(fileobj=out, mode="a")
+            stat_update = 0
+            async for msg in ctx.channel.history(limit=None):
+                stream.write(json.dumps(msg, cls=Enc).encode("utf-8"))
+                stream.write('\n'.encode("utf-8"))
+                stat_update += 1
+                if stat_update % 1000 == 0:
+                    delta = datetime.datetime.now() - msg.created_at
+                    await status.edit(content="Archiving {}, {} remaining...".format(chname, delta))
+            stream.close()
+            out.seek(0)
+            await status.delete()
+            await ctx.send(content="Archived {} containing {} messages".format(chname, stat_update),
+                           file=discord.File(out, filename="{}.gz".format(chname)))
